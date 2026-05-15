5413c5c22269745f75d424981687e61d739a48ef
Samuel Kroiß <mail@samuelkroiss.de>
2026-05-15 14:59:42 +0200

    Update ProductSeeder.php

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -14,12 +14,23 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         $products = [
-            ['name' => 'Paulaner Spezi', 'price' => 1.20, 'category' => 'Softdrinks'],
-            ['name' => 'Augustiner Hell', 'price' => 1.50, 'category' => 'Bier'],
-            ['name' => 'Club Mate', 'price' => 1.20, 'category' => 'Softdrinks'],
-            ['name' => 'Kaffee Crema', 'price' => 0.60, 'category' => 'Kaffee'],
-            ['name' => 'Snickers', 'price' => 0.80, 'category' => 'Snacks'],
-            ['name' => 'Apfelschorle', 'price' => 1.20, 'category' => 'Wasser und Säfte'],
+            ['name' => 'Apfelschorle naturtrüb 0,5l', 'price' => 1, 'category' => 'Wasser und Säfte'],
+            ['name' => 'Balisto', 'price' => 1.20, 'category' => 'Snacks'],
+            ['name' => 'Eichhofner Helles 0,5l', 'price' => 1.50, 'category' => 'Bier'],
+            ['name' => 'Eichhofner 333 0,5l', 'price' => 1.50, 'category' => 'Bier'],
+            ['name' => 'Gösser Radler 0,5l', 'price' => 1.50, 'category' => 'Bier'],
+            ['name' => 'Club Mate 0,5l', 'price' => 1.20, 'category' => 'Softdrinks'],
+            ['name' => 'Kaffee', 'price' => 0.60, 'category' => 'Kaffee'],
+            ['name' => 'Duplo', 'price' => 0.30, 'category' => 'Snacks'],
+            ['name' => 'KitKat', 'price' => 0.80, 'category' => 'Snacks'],
+            ['name' => 'Mars', 'price' => 0.80, 'category' => 'Snacks'],
+            ['name' => 'Mio Mio Mate Banana 0,5l', 'price' => 1.50, 'category' => 'Softdrinks'],
+            ['name' => 'Mio Mio Mate 0,5l', 'price' => 1.50, 'category' => 'Softdrinks'],
+            ['name' => 'Mio Mio Mate Zero 0,5l', 'price' => 1.50, 'category' => 'Softdrinks'],
+            ['name' => 'Mio Mio Cola 0,5l', 'price' => 1.50, 'category' => 'Softdrinks'],
+            ['name' => 'Paulaner Spezi 0,5l', 'price' => 1.00, 'category' => 'Softdrinks'],
+            ['name' => 'ültje Studentenfutter', 'price' => 1.20, 'category' => 'Snacks'],
+            ['name' => 'Labertaler Medium', 'price' => 0.60, 'category' => 'Wasser und Säfte'],
         ];
 
         foreach ($products as $data) {
