ec460a0259ea9080ad886e2641947af1422ae8f0
Samuel Kroiß <mail@samuelkroiss.de>
2026-05-15 15:24:09 +0200

    Add labels and auth button

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,13 +2,19 @@
     {{ $slot }}
 
     <nav
-        class="mt-auto grid w-full grid-cols-3 bg-fsim-medium px-wrapper *:flex *:h-full *:w-full *:justify-center *:py-inline"
+        class="mt-auto grid w-full grid-cols-3 bg-fsim-medium px-wrapper *:flex *:h-full *:w-full *:flex-col *:items-center *:gap-2 *:p-inline"
     >
         <button onclick="history.back()">
             <x-lucide-chevron-left />
+            Zurück
         </button>
         <a href="/">
             <x-lucide-home />
+            Startseite
         </a>
+        <button>
+            <x-lucide-log-in />
+            Anmelden
+        </button>
     </nav>
 </x-layouts.main>
