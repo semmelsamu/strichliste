15e09df25783a7d0fd9ca8336fbac18c48cbb1cd
Samuel Kroiß <mail@samuelkroiss.de>
2026-05-17 00:14:43 +0200

    Add header bar for main view

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,5 +1,35 @@
 <x-layouts.app :title="$title">
-    <header class="bg-fsim-medium px-wrapper py-inline"></header>
+    <header class="space-y-content bg-fsim-medium">
+        <div
+            class="flex items-center justify-between gap-content px-wrapper pt-wrapper"
+        >
+            <button class="button">
+                <x-lucide-user />
+                semmelsamu
+            </button>
+            <p class="mr-auto">11,40 €</p>
+            <button class="button">
+                <x-lucide-log-out />
+                Abmelden
+            </button>
+        </div>
+        <nav
+            class="flex w-full justify-evenly px-wrapper *:flex *:flex-col *:items-center *:gap-2 *:px-section *:py-inline"
+        >
+            <a class="shadow-[inset_0_-3px_0_0]">
+                <x-lucide-shopping-cart />
+                Kaufen
+            </a>
+            <a>
+                <x-lucide-coins />
+                Aufladen
+            </a>
+            <a>
+                <x-lucide-history />
+                Verlauf
+            </a>
+        </nav>
+    </header>
 
     {{ $slot }}
 </x-layouts.app>
