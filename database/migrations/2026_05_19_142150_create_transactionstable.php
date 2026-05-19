82ebc03d2f48f2a945926f929359db9367c41ba8
Samuel Kroiß <mail@samuelkroiss.de>
2026-05-19 23:39:05 +0200

    Create transaction migration

---
+++ database/migrations/2026_05_19_142150_create_transactionstable.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', places: 2);
+            $table->foreignId('from_user_id')->constrained('users');
+            $table->foreignId('to_user_id')->constrained('users');
+            $table->timestamps();
+        });
+
+        Schema::create('buy_article_transactions', function (Blueprint $table) {
+            $table->foreignId('transaction_id')->primary()->constrained('transactions');
+            $table->foreignId('article_cost_id')->constrained();
+        });
+
+        Schema::create('undo_transactions', function (Blueprint $table) {
+            $table->foreignId('transaction_id')->primary()->constrained('transactions');
+            $table->foreignId('undone_transaction_id')->constrained('transactions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buy_article_transactions');
+        Schema::dropIfExists('undo_transactions');
+        Schema::dropIfExists('transactions');
+    }
+};
