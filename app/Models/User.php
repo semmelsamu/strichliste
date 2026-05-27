c328734ea42cda30561a2de2a53d5bd11b19e4cb
Samuel Kroiß <mail@samuelkroiss.de>
2026-05-27 14:56:08 +0200

    Fix wrong cast

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'user_type' => UserType::class,
+            'type' => UserType::class,
         ];
     }
 
