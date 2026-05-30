56acd15c3befeccaa0dc55715c772fdc06d990b0
Samuel Kroiß <mail@samuelkroiss.de>
2026-05-30 14:31:55 +0200

    Add auth middleware to app

--- routes/web.php
+++ routes/web.php
@@ -12,67 +12,71 @@ Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/', function () {
-    return view('pages.tally-sheet.start');
-})->name('index');
+Route::middleware('auth')->group(function () {
 
-Route::get('/article-list', function () {
-    return view('pages.tally-sheet.article-list', [
-        'categories' => Category::with('articles')->get(),
-    ]);
-})->name('article-list');
+    Route::get('/', function () {
+        return view('pages.tally-sheet.start');
+    })->name('index');
 
-Route::name('tally-sheet.')->prefix('strichliste')->group(function () {
+    Route::get('/article-list', function () {
+        return view('pages.tally-sheet.article-list', [
+            'categories' => Category::with('articles')->get(),
+        ]);
+    })->name('article-list');
 
-    Route::name('auth.')->controller(AuthController::class)->group(function () {
-        Route::get('/', 'listUsers')->name('list-users');
-        Route::get('/login/{user}', 'login')->name('login');
-        Route::post('/login/{user}', 'validatePin')->name('validate-pin');
-        Route::get('/register', 'registerForm')->name('show-register');
-        Route::post('/register', 'register')->name('register');
-        Route::get('/user-settings/{user}', 'settings')->name('show-settings');
-        Route::post('/user-settings/{user}/username', 'updateUsername')->name('update-username');
-        Route::post('/user-settings/{user}/pin', 'updatePin')->name('update-pin');
-        Route::post('/user-settings/{user}/remove-pin', 'removePin')->name('remove-pin');
+    Route::name('tally-sheet.')->prefix('strichliste')->group(function () {
+
+        Route::name('auth.')->controller(AuthController::class)->group(function () {
+            Route::get('/', 'listUsers')->name('list-users');
+            Route::get('/login/{user}', 'login')->name('login');
+            Route::post('/login/{user}', 'validatePin')->name('validate-pin');
+            Route::get('/register', 'registerForm')->name('show-register');
+            Route::post('/register', 'register')->name('register');
+            Route::get('/user-settings/{user}', 'settings')->name('show-settings');
+            Route::post('/user-settings/{user}/username', 'updateUsername')->name('update-username');
+            Route::post('/user-settings/{user}/pin', 'updatePin')->name('update-pin');
+            Route::post('/user-settings/{user}/remove-pin', 'removePin')->name('remove-pin');
+        });
+
+        Route::post('/deposit', [TransactionController::class, 'depositMoney'])->name('deposit-action');
+
+        Route::post('/buy-article', [TransactionController::class, 'buyArticle'])->name('buy-article-action');
+
+        Route::post('/undo', [TransactionController::class, 'undoTransaction'])->name('undo-transaction-action');
+
+        Route::prefix('{user}')->group(function () {
+
+            Route::get('/buy', function (User $user) {
+                return view('pages.tally-sheet.buy-overview', [
+                    'categories' => Category::all(),
+                    'user' => $user,
+                ]);
+            })->name('buy-overview');
+
+            Route::get('/buy/category/{category_id}', function (User $user, $category_id) {
+                return view('pages.tally-sheet.buy-category', [
+                    'category' => Category::with('articles')->firstWhere('id', $category_id),
+                    'user' => $user,
+                ]);
+            })->name('buy-categories');
+
+            Route::get('/deposit', function (User $user) {
+                return view('pages.tally-sheet.deposit', [
+                    'user' => $user,
+                ]);
+            })->name('deposit');
+
+            Route::get('/history', function (User $user) {
+                return view('pages.tally-sheet.history', [
+                    'normalizedTransactions' => $user->transactions()
+                        ->orderBy('created_at', 'desc')
+                        ->orderBy('id', 'desc')
+                        ->get()
+                        ->map(fn ($t) => Transaction::normalize($t)),
+                    'user' => $user,
+                ]);
+            })->name('history');
+        });
     });
 
-    Route::post('/deposit', [TransactionController::class, 'depositMoney'])->name('deposit-action');
-
-    Route::post('/buy-article', [TransactionController::class, 'buyArticle'])->name('buy-article-action');
-
-    Route::post('/undo', [TransactionController::class, 'undoTransaction'])->name('undo-transaction-action');
-
-    Route::prefix('{user}')->group(function () {
-
-        Route::get('/buy', function (User $user) {
-            return view('pages.tally-sheet.buy-overview', [
-                'categories' => Category::all(),
-                'user' => $user,
-            ]);
-        })->name('buy-overview');
-
-        Route::get('/buy/category/{category_id}', function (User $user, $category_id) {
-            return view('pages.tally-sheet.buy-category', [
-                'category' => Category::with('articles')->firstWhere('id', $category_id),
-                'user' => $user,
-            ]);
-        })->name('buy-categories');
-
-        Route::get('/deposit', function (User $user) {
-            return view('pages.tally-sheet.deposit', [
-                'user' => $user,
-            ]);
-        })->name('deposit');
-
-        Route::get('/history', function (User $user) {
-            return view('pages.tally-sheet.history', [
-                'normalizedTransactions' => $user->transactions()
-                    ->orderBy('created_at', 'desc')
-                    ->orderBy('id', 'desc')
-                    ->get()
-                    ->map(fn ($t) => Transaction::normalize($t)),
-                'user' => $user,
-            ]);
-        })->name('history');
-    });
 });
