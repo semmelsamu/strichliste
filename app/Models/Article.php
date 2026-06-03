49046214f190689cc1be8c50631b85511755cf4a
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-03 14:35:54 +0200

    Use soft deletes for articles

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
+    use SoftDeletes;
+
     public function prices(): HasMany
     {
         return $this->hasMany(ArticlePrice::class);
