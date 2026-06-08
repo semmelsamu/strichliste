9b50a83ff175e3f1d21a6a746411db69ca63a62e
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-08 22:32:15 +0200

    Flash Error Sound on Validation Failures

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->respond(function (Response $response, Throwable $exception): Response {
+            if ($exception instanceof ValidationException && $response instanceof RedirectResponse) {
+                return $response->with('sound', 'windows-error');
+            }
+
+            return $response;
+        });
     })->create();
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,3 +5,12 @@ test('the application returns a successful response', function () {
 
     $response->assertStatus(200);
 });
+
+test('validation errors flash the error sound', function () {
+    $response = $this->post(route('authenticate'));
+
+    $response
+        ->assertRedirect()
+        ->assertSessionHas('sound', 'windows-error')
+        ->assertSessionHasErrors(['name', 'password']);
+});
