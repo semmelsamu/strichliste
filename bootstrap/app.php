ce97245486379037f573a7cebfd17cd4e80395e6
DestinyofYeet <ole@ole.blue>
2026-06-09 23:26:49 +0200

    feat(nix): setup oneshort works

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 use Symfony\Component\HttpFoundation\Response;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
