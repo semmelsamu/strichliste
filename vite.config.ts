e0a74ed86e33adba8082380b40cd0919f277b2ab
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-13 20:40:53 +0200

    Load fonts fonts from google fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import laravel from "laravel-vite-plugin";
-import { local } from "laravel-vite-plugin/fonts";
+import { google } from "laravel-vite-plugin/fonts";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -9,10 +9,8 @@ export default defineConfig({
             input: ["resources/css/app.css", "resources/js/app.ts"],
             refresh: true,
             fonts: [
-                local("Rubik", {
-                    alias: "rubik",
-                    src: "resources/fonts/rubik/*.ttf",
-                    fallbacks: ["ui-sans-serif", "system-ui", "sans-serif"],
+                google("Rubik", {
+                    weights: [400, 500, 600, 700, 800],
                 }),
             ],
         }),
