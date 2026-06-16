2eeeb7f8e68c41556a52526fdd4613ce045e937e
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-16 14:48:40 +0200

    Add test tally user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $user = User::firstOrCreate(
+            ['name' => 'tally_user'],
+            [
+                'password' => Hash::make('password'),
+            ]);
+
         User::factory()->tally_user()->count(10)->create();
     }
 }
