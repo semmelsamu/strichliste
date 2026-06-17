4b7bc371c9401bb15ab32bd1b13a2e20d1997da4
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-18 01:26:39 +0200

    Refactor article display and add purchase instructions

---
+++ resources/views/components/article-image.blade.php
@@ -0,0 +1,9 @@
+@props (["article"])
+
+<div
+    {{ $attributes->class([
+        "grid aspect-video place-items-center bg-black bg-fsim-light"
+    ]) }}
+>
+    <x-lucide-file-image />
+</div>
--- resources/views/pages/tally-sheet/article-details.blade.php
+++ resources/views/pages/tally-sheet/article-details.blade.php
@@ -1,11 +1,26 @@
 <x-layouts.main title="{{ $article->name }}">
     <header class="flex items-center gap-4 bg-fsim-medium p-wrapper">
-        <a class="button" href="{{ route('tally-sheet.auth.list-users') }}">
-            <x-lucide-arrow-left />
+        <a
+            class="button bg-fsim-light"
+            href="{{ route('tally-sheet.auth.list-users') }}"
+        >
+            <x-lucide-arrow-left /> Zurück
         </a>
-        <h1>{{ $article->name }}</h1>
     </header>
-    <x-wrapper>
-        <x-currency :amount="$article->currentPrice" />
+    <x-wrapper class="flex flex-col items-center">
+        <h1>{{ $article->name }}</h1>
+        <x-currency
+            class="mt-inline mb-content text-2xl"
+            :amount="$article->currentPrice"
+        />
+        <x-article-image
+            :article="$article"
+            class="mb-section w-sm rounded-lg"
+        />
+
+        <x-note>
+            Wenn du einen Barcode hinterlegt hast, kannst du ihn jetzt scannen
+            um den Artikel zu kaufen.
+        </x-note>
     </x-wrapper>
 </x-layouts.main>
