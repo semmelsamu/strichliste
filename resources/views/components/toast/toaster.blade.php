ca684bdffb370337182a5f8dd92339fb32aae911
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-19 23:32:17 +0200

    Add toaster animations

--- resources/views/components/toast/toaster.blade.php
+++ resources/views/components/toast/toaster.blade.php
@@ -1,3 +1,3 @@
-<div class="absolute right-content bottom-content w-sm gap-2 space-y-inline">
+<div class="absolute right-content bottom-content w-sm animate-toast-in gap-2 space-y-inline">
     {{ $slot }}
 </div>
