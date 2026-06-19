c5fe4290679c6eb6ee7ead46a220d44044868448
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-19 12:34:01 +0200

    Fix database setup and SystemSoundSetting for example tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Enums\SystemSound;
+use App\Models\SystemSoundSetting;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+pest()->use(RefreshDatabase::class);
+
 test('guests are redirected from the dashboard to the login page', function () {
     $response = $this->get('/');
 
@@ -7,6 +13,11 @@ test('guests are redirected from the dashboard to the login page', function () {
 });
 
 test('validation errors flash the error sound', function () {
+    SystemSoundSetting::create([
+        'system_sound' => SystemSound::Error,
+        'sound' => 'windows-error',
+    ]);
+
     $response = $this->post(route('authenticate'));
 
     $response
