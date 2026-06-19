bb82d4756bbdce3c0a623d1c5e905bee5d67e7d6
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-19 22:51:48 +0200

    Add back button to category view

--- resources/views/pages/tally-sheet/buy-category.blade.php
+++ resources/views/pages/tally-sheet/buy-category.blade.php
@@ -1,6 +1,11 @@
 <x-layouts.tally-sheet :title="$category->name . ' kaufen'" activeTab="buy">
-    <x-wrapper class="space-y-inline">
+    <x-wrapper class="space-y-content">
+        <a href="{{ route('tally-sheet.buy-overview') }}" class="button">
+            <x-lucide-chevron-left /> Zur Übersicht
+        </a>
+
         <h2>{{ $category->name }}</h2>
+
         <div class="grid grid-cols-4 gap-inline">
             @foreach ($category->articles as $article)
                 <x-article-card :article="$article" />
