f54b6030bc18d36945e4eb0460378cfbaf239025
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-19 17:29:33 +0200

    Update article card

--- resources/views/components/article-image.blade.php
+++ resources/views/components/article-image.blade.php
@@ -1,9 +1,19 @@
 @props (["article"])
 
-<div
-    {{ $attributes->class([
-        "grid aspect-video place-items-center bg-black bg-fsim-light"
-    ]) }}
->
-    <x-lucide-file-image />
-</div>
+@if ($article->imageUrl())
+    <img
+        src="{{ $article->imageUrl() }}"
+        alt="{{ $article->name }}"
+        {{ $attributes->class([
+            "aspect-video w-full object-cover bg-fsim-light"
+        ]) }}
+    />
+@else
+    <div
+        {{ $attributes->class([
+            "grid aspect-video place-items-center bg-black bg-fsim-light"
+        ]) }}
+    >
+        <x-lucide-file-image />
+    </div>
+@endif
