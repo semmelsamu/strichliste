53d5f4169a076cbea2264a99aa07869f605f95b4
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-21 15:25:38 +0200

    Include new user table copmonent into view

--- resources/views/pages/users/index.blade.php
+++ resources/views/pages/users/index.blade.php
@@ -1,5 +1,3 @@
-@use (App\Enums\UserRole)
-
 <x-layouts.main title="Nutzer">
     <header class="flex items-center gap-4 bg-fsim-medium p-wrapper">
         <a class="button" href="{{ route('dashboard') }}">
@@ -15,52 +13,6 @@
         </a>
     </header>
     <x-wrapper>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Rollen</th>
-                    <th class="text-right">Guthaben</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($users as $user)
-                    <tr>
-                        <th
-                            @class (["w-auto", "text-text-secondary font-normal" => $user->trashed()])
-                        >
-                            {{ $user->name }}
-                        </th>
-                        <td>
-                            @foreach ($user->roles->map(fn ($role) => UserRole::tryFrom($role->name))->filter() as $role)
-                                <div class="badge">
-                                    @if ($role->icon())
-                                        @svg ($role->icon())
-                                    @endif
-                                    {{ $role->displayName() }}
-                                </div>
-                            @endforeach
-                        </td>
-                        <td class="text-right">
-                            <x-currency :amount="$user->balance" />
-                        </td>
-                        <td class="flex items-center justify-end">
-                            <a href="{{ route("users.edit", $user->id) }}">
-                                <x-lucide-square-pen />
-                            </a>
-                        </td>
-                    </tr>
-                @empty
-                    <tr>
-                        <th colspan="4" class="text-center">
-                            Keine Nutzer gefunden.
-                        </th>
-                    </tr>
-                @endforelse
-            </tbody>
-            <caption>
-                {{ sizeof($users) }} Nutzer gesamt.
-            </caption>
-        </table>
+        <livewire:users.users-table defer />
     </x-wrapper>
 </x-layouts.main>
