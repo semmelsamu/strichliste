fef4b8188be917454015f12a4f35e8dd4683d400
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-21 23:49:17 +0200

    Add version badge

--- app/helpers.php
+++ app/helpers.php
@@ -8,3 +8,16 @@ if (! function_exists('tally_session()')) {
         return app(TallySheetSessionService::class);
     }
 }
+
+if (! function_exists('app_version')) {
+    function app_version(): ?string
+    {
+        try {
+            $composer = json_decode(file_get_contents(base_path('composer.json')), true);
+
+            return $composer['version'] ?? null;
+        } catch (Throwable) {
+            return null;
+        }
+    }
+}
--- resources/views/components/env-badge.blade.php
+++ resources/views/components/env-badge.blade.php
@@ -4,4 +4,10 @@
     >
         DEV
     </span>
+@elseif ($version = app_version())
+    <span
+        class="absolute top-0 left-0 z-50 rounded-br bg-fsim-dark px-inline py-0.5 text-xs font-bold text-text-secondary ring-1 ring-fsim-light"
+    >
+        {{ $version }}
+    </span>
 @endif
