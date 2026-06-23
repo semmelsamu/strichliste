6da1f208ecd142aaee9af17353e74bdf2ecf6046
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-23 14:40:35 +0200

    Fix header overflow issue

--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -11,7 +11,7 @@
     @fonts
     @vite (['resources/css/app.css', 'resources/js/app.ts'])
 </head>
-<body class="relative mx-auto flex h-screen max-w-6xl flex-col bg-fsim-dark">
+<body class="relative mx-auto flex max-w-6xl flex-col bg-fsim-dark">
     <x-version-badge />
 
     {{ $slot }}
