bca43a068e82655f8ec1857c2518baafb29a1353
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-30 00:29:20 +0200

    Improve print views

--- resources/views/pages/tally-sheet/history.blade.php
+++ resources/views/pages/tally-sheet/history.blade.php
@@ -2,6 +2,7 @@
     <x-header.tally-sheet activeTab="history" />
 
     <main class="wrapper space-y-section px-wrapper py-section">
+        <h1 class="hidden print:block">Transaktionen</h1>
         <livewire:livewire.transaction-history defer />
     </main>
 </x-layout.main>
