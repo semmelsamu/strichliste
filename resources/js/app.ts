5185e92ecec9b78b94e625c1961a949616484729
Samuel Kroiß <mail@samuelkroiss.de>
2026-06-30 16:09:09 +0200

    Add scroll restore functinality

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,7 +1,10 @@
 import { Livewire, Alpine } from "../../vendor/livewire/livewire/dist/livewire.esm";
 import inactivityTimeout from "./components/inactivity-timeout";
 import scrollspy from "./components/scrollspy";
+import scrollRestore from "./components/scroll-restore";
 import scanner from "./scanner";
+
+scrollRestore();
 
 window.scanner = scanner;
 
